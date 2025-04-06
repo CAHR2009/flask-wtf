4b5cea02e6163f9700f9fde18647446123a55a35
Саша Чедукаев <cedukaevsasa@gmail.com>
2025-04-06 21:41:14 +0700

    2st class

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import flask
-from flask import render_template
+from flask import render_template, url_for
 
 
 app = flask.Flask(__name__)
@@ -9,6 +9,12 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 @app.route("/<title>")
 def mars(title):
     return render_template('base.html', title=title)
+
+@app.route("/training/<prof>")
+def profession(prof):
+    link_eng = url_for("static", filename="img/eng.jpg")
+    link_sient = url_for("static", filename="img/sient.jpg")
+    return render_template('profession.html', prof=prof, link_eng=link_eng, link_sient=link_sient)
 
 
 if __name__ == '__main__':
